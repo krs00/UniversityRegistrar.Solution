b4c92cf00def9566e8f29966d8f11ab42a6e1ead
Kymani Stephens <kymanist00@gmail.com>
2023-05-22 11:45:58 -0700

    add Details, Edit and Delete for Students
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>

--- UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -35,42 +35,40 @@ namespace UniversityRegistrar.Controllers
       return RedirectToAction("Index");
     }
 
-    // public ActionResult Details(int id)
-    // {
-    //   // Item thisItem = _db.Items
-    //   //                     .Include(item => item.Category)
-    //   //                     .FirstOrDefault(item => item.ItemId == id);
-    //   return View(thisItem);
-    // }
+    public ActionResult Details(int id)
+    {
+      Student thisStudent = _db.Students
+                          .FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);
+    }
 
-    // public ActionResult Edit(int id)
-    // {
-    //   // Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   // ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-    //   return View(thisItem);
-    // }
+    public ActionResult Edit(int id)
+    {
+      Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);
+    }
 
-    // [HttpPost]
-    // public ActionResult Edit(Item item)
-    // {
-    //   // _db.Items.Update(item);
-    //   // _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost] 
+    public ActionResult Edit(Student student)
+    {
+      _db.Students.Update(student); 
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult Delete(int id)
-    // {
-    //   // Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   return View(thisItem);
-    // }
+    public ActionResult Delete(int id) 
+    {
+      Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);  
+    }
 
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //   // Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   // _db.Items.Remove(thisItem);
-    //   // _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      _db.Students.Remove(thisStudent);
+      _db.SaveChanges(); 
+      return RedirectToAction("Index");
+    }
   }
 }
