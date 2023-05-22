07e05be9eb45bd081fc582594d9d0ed3fae17696
Kymani Stephens <kymanist00@gmail.com>
2023-05-22 13:27:49 -0700

    add views and routes for Course Index, Edit, Details, Delete and Create
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>

--- UniversityRegistrar/Controllers/CoursesController.cs
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -22,60 +22,53 @@ namespace UniversityRegistrar.Controllers
       return View(model);
     }
 
-    // public ActionResult Create()
-    // {
-    //   // ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-    //   return View();
-    // }
+    public ActionResult Create()
+    {
+      return View();
+    }
 
-    // [HttpPost]
-    // public ActionResult Create(Item item)
-    // {
-    //   // if (item.CategoryId == 0)
-    //   // {
-    //   //   return RedirectToAction("Create");
-    //   // }
-    //   // _db.Items.Add(item);
-    //   // _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Create(Course course)
+    {
+      _db.Courses.Add(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult Details(int id)
-    // {
-    //   // Item thisItem = _db.Items
-    //   //                     .Include(item => item.Category)
-    //   //                     .FirstOrDefault(item => item.ItemId == id);
-    //   return View(thisItem);
-    // }
+    public ActionResult Details(int id)
+    {
+      Course thisCourse = _db.Courses
+                          .FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
 
-    // public ActionResult Edit(int id)
-    // {
-    //   // Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   // ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-    //   return View(thisItem);
-    // }
+    public ActionResult Edit(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
 
-    // [HttpPost]
-    // public ActionResult Edit(Item item)
-    // {
-    //   // _db.Items.Update(item);
-    //   // _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Edit(Course course)
+    {
+      _db.Courses.Update(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult Delete(int id)
-    // {
-    //   // Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   return View(thisItem);
-    // }
+    public ActionResult Delete(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    }
 
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //   // Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   // _db.Items.Remove(thisItem);
-    //   // _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(course => course.CourseId == id);
+      _db.Courses.Remove(thisCourse);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
